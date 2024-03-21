586a78bcae37a806a6db2cbda6eb28e39312a942
Anton Kalyutich <anton.kalyutich@gmail.com>
2024-03-21 07:57:10 -0400

    Added three more practices to PRACTICES

--- shared/practices.js
+++ shared/practices.js
@@ -3,7 +3,28 @@ export const PRACTICES = [
         id: 0,
         name: 'Refuge with the Enlightened Attitude',
         image: require("../assets/images/refuge-tree.png"),
-        current: false,
+        doingNow: false,
         description: "111 111 prostrations"
+    },
+    {
+        id: 1,
+        name: 'Diamond Mind',
+        image: require("../assets/images/diamond-mind.png"),
+        doingNow: false,
+        description: '111 111 repititions of 100-sylable mantra'
+    },
+    {
+        id: 2,
+        name: 'Mandala Offering',
+        image: require("../assets/images/mandala-offering.png"),
+        doingNow: false,
+        description: '111 111 mandala offerings'
+    },
+    {
+        id: 3,
+        name: 'Guru Yoga',
+        image: require("../assets/images/guru-yoga.png"),
+        doingNow: true,
+        description: '111 111 repititions of 6-lines invocation'
     }
 ]
